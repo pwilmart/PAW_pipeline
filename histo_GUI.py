2b18d5fe9633865bc2f95d55cd8434f527437b32
pwilmart <wilmarth@ohsu.edu>
2019-01-16 16:17:23 -0800

    updated histo_GUI Powerpoint

--- histo_GUI.py
+++ histo_GUI.py
@@ -1,4 +1,33 @@
-###############################
+"""histo_GUI.py: Written by Billy Rathje, OHSU, 2014.
+Also Phil Wilmarth, OHSU.
+Library of support functions and classes for PAW pipeline programs.
+
+The MIT License (MIT)
+
+Copyright (c) 2017 Phillip A. Wilmarth and OHSU
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Direct questions to:
+Technology & Research Collaborations, Oregon Health & Science University,
+Ph: 503-494-8200, FAX: 503-494-4729, Email: techmgmt@ohsu.edu.
+"""###############################
 ###############################
 # converting to Python 3 -PW 9/16/2017
 
